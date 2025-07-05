a6ea348401b48d607af825b3143fdcc91ea40ebd
Yushar Wahidin Zamzam <yusharw@gmail.com>
2025-07-05 16:54:44 +0700

    Decrease concurrent users limit from 100 to 25 for improved resource management

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ const (
 	imagePath       = "1.jpg"
 	attendanceId    = "c76be934-07bf-4a80-a0ab-e1700594f357"
 	checkType       = "checkIn"
-	concurrentUsers = 100
+	concurrentUsers = 25
 )
 
 func sendRequest(wg *sync.WaitGroup, successCounter *uint64, failureCounter *uint64) {
