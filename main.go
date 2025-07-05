35099f15cb62f802cc6e921813f6a76f78aa8a69
Yushar Wahidin Zamzam <yusharw@gmail.com>
2025-07-05 20:13:53 +0700

    Increase concurrent users limit from 25 to 50 for improved load handling

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ const (
 	imagePath       = "1.jpg"
 	attendanceId    = "c76be934-07bf-4a80-a0ab-e1700594f357"
 	checkType       = "checkIn"
-	concurrentUsers = 25
+	concurrentUsers = 50
 )
 
 func sendRequest(wg *sync.WaitGroup, successCounter *uint64, failureCounter *uint64) {
