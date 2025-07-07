18a94022d5d5a4ecfc4819f53c584f20efba4511
Yushar Wahidin Zamzam <yusharw@gmail.com>
2025-07-07 09:33:01 +0700

    Increase concurrent users limit from 25 to 500 for enhanced load handling

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ const (
 	imagePath       = "1.jpg"
 	attendanceId    = "c76be934-07bf-4a80-a0ab-e1700594f357"
 	checkType       = "checkIn"
-	concurrentUsers = 25
+	concurrentUsers = 500
 )
 
 type Config struct {
